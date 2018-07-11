542df1823f88fcb4668f9dcac38291d47252e089
SoundBrake <mesuper222@gmail.com>
2018-07-11 15:31:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
 var currentUser = "Mary"
 var welcomeMessage = "Welcome to Klossybook, " + currentUser + "!"
-var excitedWelcomeMessage = "WELCOME TO KLOSSYBOOK, " + currentUser+ "!"
+var excitedWelcomeMessage = welcomeMessage.toUpperCase();
 var shortGreeting = "Welcome, "  + currentUser.charAt(0)  + "!"
