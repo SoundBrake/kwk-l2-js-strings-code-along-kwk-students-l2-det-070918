e483b1a917db5299e0b4964f97999ec0fc2e02eb
SoundBrake <mesuper222@gmail.com>
2018-07-11 15:27:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // Write your code in this file!
 var currentUser = "Mary"
-var welcomeMessage = "Welcome to Klossybook " + currentUser + "!"
+var welcomeMessage = "Welcome to Klossybook, " + currentUser + "!"
